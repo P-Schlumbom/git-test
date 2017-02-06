61f7f3f65549e539487660ed5e82a198d3db9f24
P-Schlumbom <paul.schlumbom@gmail.com>
2017-02-06 20:50:13 +1300

    added wit, numpy and pyowm

--- echobot/Scripts/activate.bat
+++ echobot/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\PaulS_000\echoserver\echobot"
+set "VIRTUAL_ENV=C:\Users\PaulS_000\echoserver-wit\echobot"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
